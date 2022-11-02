0782b2d647ef0235113e617c5b4038fe1dd21415
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-11-02 09:57:13 -0400

    issues with Heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const htmlRoutes = require("./routes/htmlroutes");
 const apiRoutes = require("./routes/apiroutes")
 
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
