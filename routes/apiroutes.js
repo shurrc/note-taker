88277a42284f31efde4149b7b833204079a7b3c8
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-11-01 20:42:30 -0400

    added comments and deleted unused helper file

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,33 +2,31 @@ const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
 const uuid = require("../helper/uuid")
-const utils = require("../helper/fsUtils")
 
-router.get("/notes", ( req, res) => {
+
+router.get("/notes", ( req, res) => {  //get route
     const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
     console.log(typeof savedNotes)
     res.status(200).json(savedNotes) 
 });
-//req.body to create new object push object into savednotes
-router.post("/notes", (req, res) => {
 
-    const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
-    let newNote = {
+router.post("/notes", (req, res) => {  //Post route
+
+    const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")) //reads db.JSON and converts it into an object
+    let newNote = {  //creates new object from request object and adds id value.
         title: req.body.title,
         text: req.body.text,
-        id: uuid()
+        id: uuid() //automatically generates unique id
     }
 
-    savedNotes.push(newNote)
+    savedNotes.push(newNote)  //pushes new object into savedNotes
     fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(savedNotes))
-    console.log(req.body)
     res.status(200).json(savedNotes)
-    console.log(savedNotes)
 
     });
-router.delete("/notes/:id", ( req, res) => {
+router.delete("/notes/:id", ( req, res) => { //delete route
     const savedNotes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8"))
-    const filteredNotes = savedNotes.filter(note => note.id !== req.params.id)
+    const filteredNotes = savedNotes.filter(note => note.id !== req.params.id) //creates new object out of notes without the id of the deleted note.
     fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(filteredNotes))
     console.log(filteredNotes)
     res.status(200).json(filteredNotes) 
